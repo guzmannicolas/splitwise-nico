29cc5fd317ea4c403226b6035449c9b3889854c4
guzmannicolas <brunonicolasguzman@hotmail.com.ar>
2025-11-15 20:13:35 +1100

    new favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,7 +8,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Dividi2 - Divide gastos con amigos y mantén las cuentas claras</title>
         <meta name="description" content="Dividir gastos entre grupos de personas" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </>
